e63381731ee6ce2be160ec19a92835d4aac8d534
humaafatima <humaf1904@gmail.com>
2024-04-30 21:15:37 +0500

    Adding ATM Machine Project

---
+++ main.js
@@ -0,0 +1,67 @@
+import inquirer from "inquirer";
+let accountBalance = 45000;
+let pinCode = 5678;
+let pinReply = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinReply.pin === pinCode) {
+    console.log("Correct pin code");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["whithdraw", "check balance"]
+        }
+    ]);
+    if (operationAns.operation === "whithdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amountOptions",
+                message: "click your desired amount",
+                type: "list",
+                choices: [2000, 5000, 10000, 20000, "other amount"]
+            }
+        ]);
+        if (amountAns.amountOptions === 2000) {
+            accountBalance -= amountAns.amountOptions;
+            console.log(`Your remaining balance is ${accountBalance}`);
+        }
+        else if (amountAns.amountOptions === 5000) {
+            accountBalance -= amountAns.amountOptions;
+            console.log(`Your remaining balance is ${accountBalance}`);
+        }
+        else if (amountAns.amountOptions === 10000) {
+            accountBalance -= amountAns.amountOptions;
+            console.log(`Your remaining balance is ${accountBalance}`);
+        }
+        else if (amountAns.amountOptions === 20000) {
+            accountBalance -= amountAns.amountOptions;
+            console.log(`Your remaining balance is ${accountBalance}`);
+        }
+        else if (amountAns.amountOptions === "other amount") {
+            let otherAmount = await inquirer.prompt([
+                {
+                    name: "amount",
+                    message1: "enter your amount",
+                    type: "number"
+                }
+            ]);
+            accountBalance -= otherAmount.amount;
+            console.log(`Your remaining balance is ${accountBalance}`);
+        }
+        ;
+    }
+    if (operationAns.operation === "check balance") {
+        console.log(`Your account balance is ${accountBalance}`);
+    }
+}
+else {
+    if (pinReply.pin != pinCode)
+        (console.log("Incorrect pin code"));
+}
+;
